65e6fbed014bc233f898eaca4c25f0358acb0eee
Franco Colacilli <colacillifranco@gmail.com>
2021-04-03 12:52:59 -0300

    Build

--- src/template/CScrollbar.js
+++ src/template/CScrollbar.js
@@ -1,72 +1,75 @@
-import React, { useState, createRef, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import PerfectScrollbar from 'perfect-scrollbar'
-import 'perfect-scrollbar/css/perfect-scrollbar.css'
-import './scrollbar.css'
+import React, { useState, createRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import PerfectScrollbar from "perfect-scrollbar";
+/*import 'perfect-scrollbar/css/perfect-scrollbar.css'*/
+/*import './scrollbar.css'*/
 
 //component - CoreUI / CScrollbar
-const CScrollbar = props => {
+const CScrollbar = (props) => {
+	const {
+		tag: Tag,
+		className,
+		//
+		innerRef,
+		settings,
+		switcher,
+		...attributes
+	} = props;
 
-  const {
-    tag: Tag,
-    className,
-    //
-    innerRef,
-    settings,
-    switcher,
-    ...attributes
-  } = props
+	const [instance, setInstance] = useState();
+	const ref = createRef();
+	innerRef && innerRef(ref);
 
-  const [instance, setInstance] = useState()
-  const ref = createRef()
-  innerRef && innerRef(ref)
+	useEffect(() => {
+		switcher ? init() : uninit();
+	}, [switcher]);
 
-  useEffect(() => {
-    switcher ? init() : uninit()
-  }, [switcher])
+	useEffect(() => uninit(), []);
 
-  useEffect(() => uninit(), [])
+	const init = () => {
+		if (!instance) {
+			createPerfectScrollbar();
+		}
+	};
 
-  const init = () => {
-    if (!instance) {
-      createPerfectScrollbar()
-    }
-  }
+	const createPerfectScrollbar = () => {
+		setInstance(new PerfectScrollbar(ref.current, settings));
+	};
 
-  const createPerfectScrollbar = () => {
-    setInstance(new PerfectScrollbar(ref.current, settings))
-  }
+	const uninit = () => {
+		if (instance) {
+			instance.destroy();
+			setInstance(null);
+		}
+	};
 
-  const uninit = () => {
-    if (instance) {
-      instance.destroy()
-      setInstance(null)
-    }
-  }
-
-  // render
-  return (
-    <Tag 
-      className={classNames(className)} 
-      style={{ position: 'relative' }} 
-      {...attributes} 
-      ref={ref}
-    />
-  )
-}
+	// render
+	return (
+		<Tag
+			className={classNames(className)}
+			style={{ position: "relative" }}
+			{...attributes}
+			ref={ref}
+		/>
+	);
+};
 
 CScrollbar.propTypes = {
-  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array, PropTypes.object]),
-  //
-  settings: PropTypes.object,
-  switcher: PropTypes.bool,
-  innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func])
+	tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+	className: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.array,
+		PropTypes.object,
+	]),
+	//
+	settings: PropTypes.object,
+	switcher: PropTypes.bool,
+	innerRef: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
 
 CScrollbar.defaultProps = {
-  tag: 'div'
+	tag: "div",
 };
 
-export default CScrollbar
+export default CScrollbar;
